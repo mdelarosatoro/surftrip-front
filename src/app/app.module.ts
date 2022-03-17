9e5c5da864cb58b8b4141200761f471966402ba9
Max De La Rosa Toro <mdelarosatoroag@gmail.com>
2022-03-17 15:05:30 +0100

    Created register module and flat component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StoreModule } from '@ngrx/store';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RegisterModule } from './register/register.module';
 @NgModule({
     declarations: [AppComponent],
     imports: [
@@ -16,6 +17,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
         CoreModule,
         HttpClientModule,
         FontAwesomeModule,
+        RegisterModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-    declarations: [HeaderComponent],
+    declarations: [HeaderComponent, FooterComponent],
     imports: [CommonModule, FontAwesomeModule],
-    exports: [HeaderComponent],
+    exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
