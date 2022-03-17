ee4be615336a6d5e56b72f2f47a538e59b81af2c
Max De La Rosa Toro <mdelarosatoroag@gmail.com>
2022-03-17 13:14:12 +0100

    Generated core module and header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}, {})],
+    imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}, {}), CoreModule],
     providers: [],
     bootstrap: [AppComponent],
 })
