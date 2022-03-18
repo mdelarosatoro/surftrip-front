deaffb55c1e4485cd6fab7693f11241e80d540d7
Max De La Rosa Toro <mdelarosatoroag@gmail.com>
2022-03-18 09:24:06 +0100

    Generate menu component and placed it inside main

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,16 @@ import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FooterComponent } from './footer/footer.component';
 import { ButtonComponent } from './button/button.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-    declarations: [HeaderComponent, FooterComponent, ButtonComponent],
+    declarations: [
+        HeaderComponent,
+        FooterComponent,
+        ButtonComponent,
+        MenuComponent,
+    ],
     imports: [CommonModule, FontAwesomeModule],
-    exports: [HeaderComponent, FooterComponent, ButtonComponent],
+    exports: [HeaderComponent, FooterComponent, ButtonComponent, MenuComponent],
 })
 export class CoreModule {}
