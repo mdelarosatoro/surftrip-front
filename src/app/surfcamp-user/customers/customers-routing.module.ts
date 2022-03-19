cf808d6fe87f84b4f49882d69185b6597303d183
Max De La Rosa Toro <mdelarosatoroag@gmail.com>
2022-03-19 17:01:56 +0100

    Removed comment from customers routing module

--- src/app/surfcamp-user/customers/customers-routing.module.ts
+++ src/app/surfcamp-user/customers/customers-routing.module.ts
@@ -12,10 +12,6 @@ const routes: Routes = [
                 path: '',
                 component: CustomerListComponent,
             },
-            // {
-            //     path: 'details/:id',
-            //     component: PackageDetailsComponent,
-            // },
         ],
     },
 ];
